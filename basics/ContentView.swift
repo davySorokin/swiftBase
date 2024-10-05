36213a9b5a5a8e91460f01bdf5d756802c21c501
davySorokin <kirilldavidsorokin@gmail.com>
2024-10-05 19:32:17 +0900

    testing rule set.

--- basics/ContentView.swift
+++ basics/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("davySorokin!")
+            Text("Rule Change")
         }
         .padding()
     }
