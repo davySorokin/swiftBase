71270e38955086f15ec457c367009f789d6df7ac
davySorokin <kirilldavidsorokin@gmail.com>
2024-10-05 19:19:16 +0900

    Initial Commit

---
+++ basics/basicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicsApp.swift
+//  basics
+//
+//  Created by Kirill David Sorokin on 05/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct basicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
